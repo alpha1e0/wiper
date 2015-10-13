d18eed61a0f478e7cd3e8ad8faf2cffe5675686c
alpha1e0 <yan.shifm@foxmail.com>
2015-10-13 23:49:28 +0800

    modify searchengine.py

--- plugin/lib/searchengine.py
+++ plugin/lib/searchengine.py
@@ -14,46 +14,101 @@ import yaml
 from plugin.lib.taskManager import PluginError
 
 
-def searchConfig(name):
-	configFile = os.path.join("plugin","config","searchengine.yaml")
-	try:
-		with open(configFile, "r") as fd:
-			config = yaml.load(fd)[name]
-	except IOError:
-		raise PluginError("open searchengine configure file 'plugin/config/searchengine.yaml' failed")
-	else:
-		return config
+class SearchConfig(object):
+	def __new__(cls, engine):
+		configFile = os.path.join("plugin","config","searchengine.yaml")
+		try:
+			with open(configFile, "r") as fd:
+				config = yaml.load(fd)[engine]
+		except IOError:
+			raise PluginError("open searchengine configure file 'plugin/config/searchengine.yaml' failed")
+		else:
+			return config
 
 
 class Query(object):
 	'''
-	Build query string
+	Build query keyword
 	parameter:
-		site:
-		title:
-		url:
-		filetype:
-		ext:
-		link:
-		kw:
+		site: seach specified site
+		title: search in title
+		url: search in url
+		filetype: search files with specified file type
+		ext: search files with specified extetion
+		link: search in link
+		kw: raw keywords to search 
 	example:
-		query(site="xxx.com") - query(site="www.xxx.com")
+		query = Query(site="xxx.com") | -Query(site="www.xxx.com") | Query(kw="password")
+		query.doSearch(engine="baidu")
 	'''
 	def __init__(self, **kwargs):
+		self._qlist = list()
 		for key,value in kwargs.iteritems():
-			self.kw = "{0}={1} ".format(key,value)
+			self._qlist.append(["",key,value])
 
-	def __sub__(self, obj):
-		pass
+	def __neg__(self, obj):
+		self._qlist[0][0] = "-"
+		return self
 
-	def __and__(self, obj):
-		pass
+	def __pos__(self, obj):
+		self._qlist[0][0] = "+"
+		return self
 
 	def __or__(self, obj):
-		pass
+		self._qlist += obj._qlist
+		return self
+
+
+	def genKeyword(self, engine):
+		'''
+		Generate keyword.
+		'''
+		config = SearchConfig("baidu")
+		keyword = ""
+		for line in self._qlsit:
+			if line[1] in config['ghsyn']:
+				line[1] = config['ghsyn'][line[1]]
+				keyword += "".join(line)+" "
+				continue
+			elif line[1] == "kw":
+				keyword += line[0]+line[2]
+
+		return keyword
+
+		domain = config['url']
+		default = "&".join([k+"="+v for k,v in config['default'].iteritems()])
+		querystr = config['param']['query'] + "=" + keyword
+
+		return domain + "&" + default + "&" + querystr
 
 
 	def doSearch(engine="baidu", size=500, encode="utf-8"):
+		keyword = self.genKeyword(engine)
+		if engine == "baidu":
+			baidu = Baidu(size=size, encode=encode)
+			return baidu.search(keyword)
+		else:
+			return None
+
+
+class Baidu(object):
+	'''
+	Baidu search engine.
+	parameter:
+		size: specified the amount of the result
+		encode: specified the url encode method
+	example:
+		baidu=Baidu()
+		baidu.search("site:xxx.com password.txt")
+	'''
+	def __init__(self, size=500, encode="utf-8"):
+		self.size = size
+		self.encode = encode
+
+		self.config = SearchConfig("baidu")
+
+
+	def search(self, keyword, size=None):
 		pass
 
 
