770a87d978bf379442d43a3fd5999b5ee9311b7b
alpha1e0 <yan.shifm@foxmail>
2015-10-14 09:19:26 +0800

    bug fix

--- plugin/lib/searchengine.py
+++ plugin/lib/searchengine.py
@@ -14,47 +14,57 @@ import yaml
 from plugin.lib.taskManager import PluginError
 
 
-def searchConfig(name):
-	configFile = os.path.join("plugin","config","searchengine.yaml")
-	try:
-		with open(configFile, "r") as fd:
-			config = yaml.load(fd)[name]
-	except IOError:
-		raise PluginError("open searchengine configure file 'plugin/config/searchengine.yaml' failed")
-	else:
-		return config
+class SearchConfig(object):
+	def __new__(cls, engine):
+		configFile = os.path.join("plugin","config","searchengine.yaml")
+		try:
+			with open(configFile, "r") as fd:
+				config = yaml.load(fd)[engine]
+		except IOError:
+			raise PluginError("open searchengine configure file 'plugin/config/searchengine.yaml' failed")
+		else:
+			return config
 
 
 class Query(object):
 	'''
 	Build query string
 	parameter:
-		site:
-		title:
-		url:
-		filetype:
-		ext:
-		link:
-		kw:
+		site: search in specified site
+		title: search in title
+		url: search in url
+		filetype: search file with specified file type
+		ext: search file with specified filet extention
+		link: search in link
+		kw: search keywords
 	example:
-		query(site="xxx.com") - query(site="www.xxx.com")
+		query(site="xxx.com") | -query(site="www.xxx.com")
 	'''
 	def __init__(self, **kwargs):
+		self._qlist = list()
 		for key,value in kwargs.iteritems():
-			self.kw = "{0}={1} ".format(key,value)
+			self._qlist.append(["",key,value])
 
-	def __sub__(self, obj):
-		pass
+	def __neg__(self):
+		self._qlist[0][0] = "-"
+		return self
 
-	def __and__(self, obj):
-		pass
+	def __pos__(self):
+		self._qlist[0][0] = "+"
+		return self
 
 	def __or__(self, obj):
-		pass
+		self._qlist += obj._qlist
+		return self
+
+	def genUrl(self, engine):
+		config = SearchConfig(engine)
+		for line in self._qlist:
+			for item in line:
 
 
 	def doSearch(engine="baidu", size=500, encode="utf-8"):
-		pass
+		url = self.genUrl(engine)
 
 
 
