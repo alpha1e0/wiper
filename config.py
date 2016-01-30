ccf3d6558a795c455a0dd076999cb9cbb7aaad6f
alpha1e0 <yan.shifm@foxmail.com>
2016-01-30 21:01:27 +0800

    bug fix

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: UTF-8 -*-
+#-*- coding:utf-8 -*-
 
 '''
 Wiper, an assistant tool for web penetration test.
@@ -46,6 +46,7 @@ class Log(object):
     input:
         logname: specify the logname
         toConsole: whether outputing to console
+        toFile: whether to logging to file
     '''
     def __new__(cls, logname=None, toConsole=True, toFile="wiper"):
         if not os.path.exists("log"):
